1cb21ba28240346e850a0ef83105d6be10b5eff2
makoto abe <abe00makoto@gmail.com>
2017-09-11 19:41:25 +0900

    add login message model

--- models/model.go
+++ models/model.go
@@ -33,9 +33,9 @@ type Room struct {
 
 // Message はチャットのメッセージを示します。Roomが親になります
 type Message struct {
-	ID      int64         `datastore:"-" goon:"id"`
-	Room    datastore.Key `datastore:"-" goon:"parent"`
-	Auther  datastore.Key
+	ID      int64          `datastore:"-" goon:"id"`
+	Room    *datastore.Key `datastore:"-" goon:"parent"`
+	Auther  *datastore.Key
 	Content string `datastore:",noindex"`
 	Created time.Time
 }
@@ -51,11 +51,21 @@ type Tag struct {
 type Reader interface {
 	Rooms(offset, limit int) ([]*Room, error)
 	Room(name string) (*Room, error)
+
+	Messages(roomName string, offset, limit int) ([]*Message, error)
+	Message(roomName string, messageID int64) (*Message, error)
+
+	Login(googleUserID string) (*Login, error)
 }
 
 type Writer interface {
 	RunInTransaction(f func(tg *Model) error, opts *datastore.TransactionOptions) error
+
 	PostRoom(name, description string) (*datastore.Key, error)
+
+	PostMessage(roomName, googleUserID, content string) (*datastore.Key, error)
+
+	PostLogin(googleUserID, email, name string, picture []byte) (*datastore.Key, error)
 }
 
 type Model struct {
@@ -157,5 +167,88 @@ func (m *Model) PostRoom(name, description string) (*datastore.Key, error) {
 		return nil, err
 	}
 
+	return key, nil
+}
+
+func (m *Model) RoomKey(roomName string) *datastore.Key {
+	return datastore.NewKey(m.appEngineContext, "Room", roomName, 0, nil)
+}
+
+func (m *Model) LoginKey(googleUserID string) *datastore.Key {
+	return datastore.NewKey(m.appEngineContext, "Login", googleUserID, 0, nil)
+}
+
+// Message is get Message
+func (m *Model) Message(roomName string, messageID int64) (*Message, error) {
+
+	message := &Message{
+		ID:   messageID,
+		Room: m.RoomKey(roomName),
+	}
+
+	err := m.Get(message)
+	if err != nil {
+		return nil, err
+	}
+	return message, nil
+}
+
+func (m *Model) Messages(roomName string, offset, limit int) ([]*Message, error) {
+	var messages []*Message
+	query := datastore.NewQuery("Message").Ancestor(m.RoomKey(roomName))
+	_, err := m.GetAll(query, &messages)
+	if err != nil {
+		return nil, err
+	}
+
+	if limit > len(messages) {
+		limit = len(messages)
+	}
+	if offset > len(messages) {
+		offset = 0
+	}
+
+	return messages[offset:limit], nil
+}
+
+func (m *Model) PostMessage(roomName, googleUserID, content string) (*datastore.Key, error) {
+	roomKey := m.RoomKey(roomName)
+	message := &Message{
+		Room:    roomKey,
+		Auther:  m.LoginKey(googleUserID),
+		Content: content,
+		Created: time.Now(),
+	}
+	key, err := m.Put(message)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
+
+func (m *Model) Login(googleUserID string) (*Login, error) {
+	login := &Login{
+		GoogleLoginID: googleUserID,
+	}
+	err := m.Get(login)
+	if err != nil {
+		return nil, err
+	}
+	return login, nil
+}
+func (m *Model) PostLogin(googleUserID, email, name string, picture []byte) (*datastore.Key, error) {
+
+	newLogin := &Login{
+		GoogleLoginID: googleUserID,
+		Email:         email,
+		Name:          name,
+		LastLogin:     time.Now(),
+		Picture:       picture,
+	}
+	key, err := m.Put(newLogin)
+	if err != nil {
+		return nil, err
+	}
 	return key, nil
 }
--- models/model_test.go
+++ models/model_test.go
@@ -3,12 +3,6 @@ package models
 import (
 	"fmt"
 	"testing"
-	"time"
-
-	"google.golang.org/appengine/datastore"
-
-	"github.com/mjibson/goon"
-	"google.golang.org/appengine/aetest"
 )
 
 func RunInTesting(t *testing.T, f func(m *Model, t *testing.T) error) {
@@ -59,33 +53,75 @@ func TestRooms(t *testing.T) {
 
 	})
 }
-func TestLoginModel(t *testing.T) {
-	c, done, err := aetest.NewContext()
-	if err != nil {
-		t.Fatalf("Could not start aetest - %v", err)
-	}
-	defer done()
-	g := goon.FromContext(c)
-	login := Login{
-		GoogleLoginID: "someID001",
-		Email:         "some@Email.com",
-		LastLogin:     time.Date(2017, 7, 1, 0, 0, 0, 0, time.UTC),
-		Name:          "someName",
-		Picture:       nil,
-	}
 
-	key, err := g.Put(&login)
-	if err != nil {
-		t.Fatalf("%v", err)
-	}
-
-	fmt.Println("key", key)
-
-	logins := []Login{}
-	_, err = g.GetAll(datastore.NewQuery("Login"), &logins)
-	if err != nil {
-		t.Fail()
-	}
-
-	fmt.Printf("%v\n", logins)
+func TestPostLogin(t *testing.T) {
+	RunInTesting(t, func(m *Model, t *testing.T) error {
+		key, err := m.PostLogin("gid01", "email@email.com", "name01", []byte("byte smaple"))
+		if err != nil {
+			return err
+		}
+		_ = key
+		return nil
+	})
 }
+
+func TestPostMessage(t *testing.T) {
+	RunInTesting(t, func(m *Model, t *testing.T) error {
+		key, err := m.PostRoom("room01", "description 01")
+		if err != nil {
+			return err
+		}
+		m.ForceApply(key)
+
+		key, err = m.PostLogin("gid01", "email@email.com", "name01", []byte("byte smaple"))
+		if err != nil {
+			return err
+		}
+		m.ForceApply(key)
+
+		key, err = m.PostMessage("room01", "gid01", "content sample01")
+		if err != nil {
+			return err
+		}
+		m.ForceApply(key)
+
+		mes, err := m.Messages("room01", 0, 100)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("%#v\n", mes[0])
+		return nil
+	})
+}
+
+// func TestLoginModel(t *testing.T) {
+// 	c, done, err := aetest.NewContext()
+// 	if err != nil {
+// 		t.Fatalf("Could not start aetest - %v", err)
+// 	}
+// 	defer done()
+// 	g := goon.FromContext(c)
+// 	login := Login{
+// 		GoogleLoginID: "someID001",
+// 		Email:         "some@Email.com",
+// 		LastLogin:     time.Date(2017, 7, 1, 0, 0, 0, 0, time.UTC),
+// 		Name:          "someName",
+// 		Picture:       nil,
+// 	}
+
+// 	key, err := g.Put(&login)
+// 	if err != nil {
+// 		t.Fatalf("%v", err)
+// 	}
+
+// 	fmt.Println("key", key)
+
+// 	logins := []Login{}
+// 	_, err = g.GetAll(datastore.NewQuery("Login"), &logins)
+// 	if err != nil {
+// 		t.Fail()
+// 	}
+
+// 	fmt.Printf("%v\n", logins)
+// }
